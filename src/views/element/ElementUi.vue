b6cb24f69d3c0cf2e9bafde30521eabde6bee2b8
cocoTang <15651430850@163.com>
2018-05-28 09:20:31 +0800

    test

--- src/views/element/ElementUi.vue
+++ src/views/element/ElementUi.vue
@@ -32,6 +32,51 @@
 
     <datetimepicker></datetimepicker>
 
+
+    <!-- 横向导航栏 -->
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
+<div class="line"></div>
+<el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
+
   <!-- tab栏实现 -->
   <!-- <ul>
       <li v-for="(item,index) in tabs" :key="index"
@@ -97,7 +142,9 @@ export default {
       tabContents: ["内容一", "内容二", "内容三"],
       num: 1,
       /* datetimepicker */
-      value6: ""
+      value6: "",
+      activeIndex: '1',
+      activeIndex2: '1'
     };
   },
   components: {
@@ -122,6 +169,9 @@ export default {
       }
       return '';
     },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   },
   created() {
     axios.interceptors.request.use(config => {
